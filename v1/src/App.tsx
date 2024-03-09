7b71236173de002a8fdbb0817950c51c0c4b21cc
mryan-3 <malawaryan@gmail.com>
2024-03-09 08:44:42 +0300

    Initialized the project with tailwind and shadcn

---
+++ v1/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-primary w-full h-screen">
+    <h1 className="text-3xl text-white text-center">Hello world Jones</h1>
+    <Button className="bg-foreground block items-center">Click me bro</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
